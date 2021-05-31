a4ed47006c0ece971d8232f196751a09ffbe89b0
Elliot Ruiz Sánchez <elliotruizs@ieee.org>
2021-05-31 18:06:13 -0500

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,50 @@
 #include "fsl_port.h"
 #include "fsl_common.h"
 
+#define PIN4      	4u
+#define	PIN6		6u
+#define PIN21      	21u
+#define	PIN22		22u
+#define PIN26		26u
+
 int main(void) {
+
+	const port_pin_config_t porta_pin4_config = {
+		    kPORT_PullUp,                                            /* Internal pull-up resistor is enabled */
+		    kPORT_FastSlewRate,                                      /* Fast slew rate is configured */
+		    kPORT_PassiveFilterDisable,                              /* Passive filter is disabled */
+		    kPORT_OpenDrainDisable,                                  /* Open drain is disabled */
+		    kPORT_HighDriveStrength,                                 /* High drive strength is configured */
+		    kPORT_MuxAsGpio,                                         /* Pin is configured as PTA4 */
+		    kPORT_UnlockRegister                                     /* Pin Control Register fields [15:0] are not locked */
+		  };
+
+	const port_pin_config_t portc_pin6_config = {
+			kPORT_PullUp,                                            /* Internal pull-up resistor is enabled */
+			kPORT_FastSlewRate,                                      /* Fast slew rate is configured */
+			kPORT_PassiveFilterDisable,                              /* Passive filter is disabled */
+			kPORT_OpenDrainDisable,                                  /* Open drain is disabled */
+			kPORT_HighDriveStrength,                                 /* High drive strength is configured */
+			kPORT_MuxAsGpio,                                         /* Pin is configured as PTA4 */
+			kPORT_UnlockRegister                                     /* Pin Control Register fields [15:0] are not locked */
+			  };
+
+	// Habilitamos los relojes de los Puertos a usar
+
+	CLOCK_EnableClock(kCLOCK_PortA);				// Switch 3
+	CLOCK_EnableClock(kCLOCK_PortB);				// Red and Blue LED
+	CLOCK_EnableClock(kCLOCK_PortC);				// Switch 2
+	CLOCK_EnableClock(kCLOCK_PortE);				// Green LED
+
+	//  Funcion de GPIO
+
+	PORT_SetPinMux(PORTB, PIN21, kPORT_MuxAsGpio); 	// Blue LED GPIO
+	PORT_SetPinMux(PORTB, PIN22, kPORT_MuxAsGpio); 	// Red LED GPIO
+	PORT_SetPinMux(PORTE, PIN26, kPORT_MuxAsGpio); 	// Green LED GPIO
+	PORT_SetPinMux(PORTA, PIN4, kPORT_MuxAsGpio); 	// Switch 3 GPIO
+	PORT_SetPinMux(PORTC, PIN6, kPORT_MuxAsGpio); 	// Switch 2 GPIO
+
+
 
     while(1){
 
